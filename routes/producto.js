da6e97f24b97585a432a1d50f551df0d3662c7e5
cccastro09 <tecnocastro2014@gmail.com>
2019-02-05 12:48:42 -0500

    hjh

--- routes/producto.js
+++ routes/producto.js
@@ -12,4 +12,45 @@ router.get('/', (req, res) => {
 
 });
 
+router.get('/ingreso', (req, res) => {
+  res.render('producto/ingreso' );
+});
+
+router.post('/guardar', (req, res) => {
+
+    knex('productos')
+      .returning('id')
+      .insert({tipo : req.body.tipo, fechacaducidad : req.body.fechacaducidad, nombre: req.body.nombre, fechaproduccion: req.body.fechaproduccion, precio: req.body.precio})
+      .then(ids =>  {
+        const id = ids[0];
+        res.redirect(`/`);
+
+  });
+});
+
+
+
+router.delete('/:id',(req,res)=>{
+  const id=req.params.id;
+  console.log('deleting...');
+
+ if(typeof id != 'undefined'){
+    knex('productos')
+      .where('id',id)
+      .del()
+      .then(productos => {
+        console.log('delete id: '+id);
+        res.redirect('/');
+    });
+
+  }else{
+
+    console.log('error invalid delete ');
+    res.status(500);
+    res.render('error', {
+      message: 'Invalid ID delete '
+    });
+  }
+});
+
 module.exports = router;
